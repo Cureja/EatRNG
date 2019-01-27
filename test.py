10e53433d63b43bd095273a38cd0f4886c4bbceb
Donny Wu <donny@roomvu.com>
2019-01-26 17:33:02 -0800

    removed

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import populartimes
 
-thing = populartimes.get_id("AIzaSyDHl55PN4e6x685QzYVmKTXEMuiQP1FrNY","ChIJSYuuSx9awokRyrrOFTGg0GY")
+thing = populartimes.get_id("","ChIJSYuuSx9awokRyrrOFTGg0GY")
 
 print(thing)
 
