6054ec6a3833675ed82e4dd144ee3946db92e72a
hoalvinh <hoalvinh@sfu.ca>
2019-01-27 09:55:03 -0800

    updated port number

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = 3000;
+const port = 80;
 
 
 
